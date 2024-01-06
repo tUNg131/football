86db8ccc9019c03ca60046554011614125f7c3b4
Tung <halethuy.hlt@gmail.com>
2024-01-06 20:06:30 +0700

    fix bugs

--- model.py
+++ model.py
@@ -36,7 +36,7 @@ class Embedding(nn.Module):
 
         # space embedding
         space_emb = self.space_embedding(
-            torch.arange(d_x * n_joint, dtype=torch.int)
+            torch.arange(d_x * n_joint, dtype=torch.int, device=x.device)
             .repeat(timesteps)
             .view(bsize, -1)
         )
